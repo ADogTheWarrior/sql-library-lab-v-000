9c2cea72b5b40e9bd11a6137b4f8529ba89f2dfa
Adam Fudge <coolfudge@gmail.com>
2017-11-09 21:34:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, charcters.species FROM series
+  "SELECT series.title, characters.species FROM series
   JOIN characters ON characters.series_id = characters.id"
 end
 

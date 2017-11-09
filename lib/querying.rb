ddb98251c611224e61018ea48a0047a7926635d4
Adam Fudge <coolfudge@gmail.com>
2017-11-09 21:31:50 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT title FROM series
   JOIN characters ON characters.series_id = characters.id
-  ORDER BY COUNT(species) DESC"
+  ORDER BY COUNT(species)"
 end
 
 def select_character_names_and_number_of_books_they_are_in

b02f469a664dd70cdbc00b3d00a03efc910a8a48
Adam Fudge <coolfudge@gmail.com>
2017-11-09 21:24:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM books"
+  "SELECT authors.name, subgenres.name FROM series
+  JOIN authors ON authors.id = series.author_id
+  JOIN subgenres ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters

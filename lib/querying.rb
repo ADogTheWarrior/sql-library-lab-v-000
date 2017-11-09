66296b2c99498acee944196b1557d46032e61f6c
Adam Fudge <coolfudge@gmail.com>
2017-11-09 22:12:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,5 +33,5 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name FROM character_books
   JOIN characters ON characters.id = character_books.characters_id
-  JOIN books ON subgenres.id = character_books.books_id"
+  JOIN books ON books.id = character_books.books_id"
 end
